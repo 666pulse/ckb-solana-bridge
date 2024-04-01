18c251d89aa435f1c1aac14c9990791e16e7ff4c
TwilightLogic <twilightlogic@gmail.com>
2024-04-01 12:41:07 +0800

    feat: add Phantom wallet

--- sol-bridge-website/src/App.tsx
+++ sol-bridge-website/src/App.tsx
@@ -1,13 +1,42 @@
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from '@solana/wallet-adapter-react'
+
+import { MainPage } from '@/pages/main-page'
 import { Layout } from '@/components/Layout'
 
-import { MainPage } from './pages/main-page'
-
 import '@/styles/global.css'
+import { useMemo, useState } from 'react'
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base'
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets'
+import { clusterApiUrl } from '@solana/web3.js'
+
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui'
 
 export const App = () => {
+  const [network] = useState(WalletAdapterNetwork.Testnet)
+  const endpoint = useMemo(() => clusterApiUrl(network), [network])
+
+  const wallets = useMemo(
+    () => [new PhantomWalletAdapter()],
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [network]
+  )
+
+  // const handleChangeNetwork = (e) => {
+  //   setNetwork(e)
+  // }
+
   return (
-    <Layout>
-      <MainPage />
-    </Layout>
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <Layout>
+            <MainPage />
+          </Layout>
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
   )
 }
---
+++ sol-bridge-website/src/components/wallet-connect.tsx
@@ -0,0 +1,7 @@
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'
+
+import('@solana/wallet-adapter-react-ui/styles.css')
+
+export const WalletConnect = () => {
+  return <WalletMultiButton />
+}
