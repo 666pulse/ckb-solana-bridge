14c351e872415ab89f5c63ce195fb1358d32fa0c
TwilightLogic <twilightlogic@gmail.com>
2024-04-02 11:45:30 +0800

    refactor: update border color on `wallet-connect`

--- sol-bridge-website/src/components/wallet-connect.tsx
+++ sol-bridge-website/src/components/wallet-connect.tsx
@@ -13,7 +13,7 @@ export const WalletConnect = () => {
   return (
     <div className="flex items-center justify-end w-full text-white">
       <Select>
-        <SelectTrigger className="w-[165px] border-[#522da8] hover:border-gray-800 focus:ring-0 mr-3 rounded-sm">
+        <SelectTrigger className="w-[125px] border-gray-700 focus:ring-0 mr-3 rounded-sm">
           <SelectValue placeholder="Select a newtork" />
         </SelectTrigger>
         <SelectContent className="font-semibold rounded-lg">
