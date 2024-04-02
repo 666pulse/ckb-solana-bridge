5cacc54ad8e0fffaa6b10b5398c76edfed5e8560
TwilightLogic <twilightlogic@gmail.com>
2024-04-02 22:42:56 +0800

    feat: get associated address info

--- sol-bridge-website/src/App.tsx
+++ sol-bridge-website/src/App.tsx
@@ -13,6 +13,8 @@ import { MainPage } from '@/pages/main-page'
 
 import { Connection, PublicKey } from '@solana/web3.js'
 
+import { getAssociatedTokenAddressSync } from '@solana/spl-token'
+
 import '@/styles/global.css'
 
 export const App = () => {
@@ -21,20 +23,21 @@ export const App = () => {
   const endpoint = useMemo(() => clusterApiUrl(network), [network])
   const wallets = useMemo(() => [new PhantomWalletAdapter()], [])
 
-  // TODO: Analyze the account
   const handleGetBalance = async () => {
-    const solana = new Connection('https://api.devnet.solana.com')
+    const connection = new Connection('https://api.devnet.solana.com')
     const accountPublicKey = new PublicKey(
       '14XaUVhoLCcHL2dZj62yvwnvhJRi1ir6x9orSGPPpaen'
     )
-    const mintAccount = new PublicKey(
+    const mintAccountPublickey = new PublicKey(
       'ssSjvvxJQddW9EgBE7CbEW64iQkUPDxU7AMqicvDqfQ'
     )
-    const account = await solana.getTokenAccountsByOwner(accountPublicKey, {
-      mint: mintAccount,
-    })
+    const ata = getAssociatedTokenAddressSync(
+      mintAccountPublickey,
+      accountPublicKey
+    )
+    const accountInfo = await connection.getAccountInfo(ata)
 
-    console.log(account)
+    console.log(accountInfo)
   }
 
   useEffect(() => {
