631a446814d5de73517ff77db8d2c581498be437
TwilightLogic <twilightlogic@gmail.com>
2024-04-03 14:31:31 +0800

    refactor: split the footer as a component

--- sol-bridge-website/src/pages/main-page.tsx
+++ sol-bridge-website/src/pages/main-page.tsx
@@ -1,6 +1,6 @@
+import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
 import { SwapCard } from '@/components/card/swap-card'
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 
 export const MainPage = () => {
   return (
@@ -45,32 +45,7 @@ export const MainPage = () => {
         </div>
 
         <div className="text-white flex flex-col items-center justify-end h-full mt-20">
-          <span className="text-muted-foreground font-black text-3xl flex items-center justify-center mb-6">
-            Made by
-          </span>
-          <div className="flex items-center justify-center gap-32">
-            <div className="flex flex-col items-center justify-center gap-1">
-              <Avatar className="h-14 w-14">
-                <AvatarImage src="/logic.jpg" alt="Logic" />
-                <AvatarFallback>Lo</AvatarFallback>
-              </Avatar>
-              <span className="text-muted-foreground font-bold">Logic</span>
-            </div>
-            <div className="flex flex-col items-center justify-center gap-1">
-              <Avatar className="h-14 w-14">
-                <AvatarImage src="/wuyi.jpg" alt="Logic" />
-                <AvatarFallback>WY</AvatarFallback>
-              </Avatar>
-              <span className="text-muted-foreground font-bold">毋意</span>
-            </div>
-            <div className="flex flex-col items-center justify-center gap-1">
-              <Avatar className="h-14 w-14">
-                <AvatarImage src="/shooter.jpg" alt="Logic" />
-                <AvatarFallback>SH</AvatarFallback>
-              </Avatar>
-              <span className="text-muted-foreground font-bold">Shooter</span>
-            </div>
-          </div>
+          <Footer />
         </div>
       </div>
     </div>
