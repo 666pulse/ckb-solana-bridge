724ee817fa9abd995fc7cd6d3ab0c53fa43dc373
TwilightLogic <twilightlogic@gmail.com>
2024-04-03 13:43:23 +0800

    feat: improve the input UI

--- sol-bridge-website/src/components/card/components/swap-to-input.tsx
+++ sol-bridge-website/src/components/card/components/swap-to-input.tsx
@@ -1,12 +1,5 @@
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from '@/components/ui/select'
 
 export const SwapToInput = () => {
   return (
@@ -26,48 +19,10 @@ export const SwapToInput = () => {
                 type="text"
                 name="name"
                 id="name"
-                className="border-0 p-0 text-indigo-600 placeholder:text-indigo-400 focus:outline-none sm:text-lg sm:leading-6 mt-2 font-semibold bg-gray-50 focus-visible:ring-0 shadow-none w-[275px]"
+                className="border-0 p-0 text-indigo-600 placeholder:text-indigo-400 focus:outline-none sm:text-lg sm:leading-6 mt-2 font-semibold bg-gray-50 focus-visible:ring-0 shadow-none w-[275px] overflow-hidden whitespace-nowrap truncate"
                 placeholder="CKB Address"
               />
             </div>
-            <Select>
-              <SelectTrigger className="w-[95px] rounded-full text-gray-800 bg-white font-medium">
-                <SelectValue
-                  placeholder={
-                    <div className="flex items-center justify-start gap-2">
-                      <img
-                        src="/nervos-network-ckb-logo.svg"
-                        alt=""
-                        className="h-3 w-auto"
-                      />
-                      <p>CKB</p>
-                    </div>
-                  }
-                />
-              </SelectTrigger>
-              <SelectContent className="font-medium">
-                <SelectItem value="ckb">
-                  <div className="flex items-center justify-start gap-2.5">
-                    <img
-                      src="/nervos-network-ckb-logo.svg"
-                      alt=""
-                      className="h-3 w-auto"
-                    />
-                    <p>CKB</p>
-                  </div>
-                </SelectItem>
-                {/* <SelectItem value="solana">
-                  <div className="flex items-center justify-start gap-2">
-                    <img
-                      src="/solana-sol-logo.svg"
-                      alt=""
-                      className="h-3 w-auto"
-                    />
-                    <p>SOL</p>
-                  </div>
-                </SelectItem> */}
-              </SelectContent>
-            </Select>
           </div>
         </div>
       </div>
